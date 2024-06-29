81c84b9e99e814981be70709d519573703c33fde
shaimaayosef <shimaa.sayed8888@gmail.com>
2024-06-30 02:08:12 +0300

    task-1

---
+++ problemSolving/README.md
@@ -0,0 +1,40 @@
+# Problem Solving Using Javascript Programming Language-DPEI
+## Basics
+
+### Lv1l:
+1. Write an algorithm to input two numbers and find their sum.
+2. Write an algorithm to input three numbers and find their average.
+### Lvl2:
+1. Write an algorithm that determines the flying time between two cities given the distance between them and the average speed of the airplane.
+2. Write an algorithm that calculates your BMI given your weight and height.The BMI is calculated using the weight divided by the height squared, where the weight in kg and height in m.
+3. Write an algorithm to enter the length in centimeters and convert it to meters and kilometers.
+4. Write an algorithm to enter data in bytes and converts it in bit and gigabytes.
+5. Write an algorithm that calculates the time needed to upload a file given the upload speed and file size.
+6. Write an algorithm that calculates the time needed to upload a file in seconds given the upload speed in Mbps and file size in GB.
+7. Given the radius of circle, determine the area and circumference.
+### Lvl3:
+1. Write an algorithm that takes as inputs two numbers and swaps their values.
+
+## Conditions Sheet
+
+### Lvl1:
+1. Write an algorithm to input two numbers and print the bigger number.
+2. Write an algorithm to check whether a number is negative, positive or zero.
+### Lvl2:
+1. Write an algorithm to check whether a number is divisible by 5 and 11 or not.
+2. Write an algorithm to check whether a number is even or odd.
+3. Write an algorithm to check whether a number is within range or not. Given two numbers that represent the range.
+### Lvl3:
+1. Write an algorithm to calculate a person's BMI and check whether a person is underweight, overweight, or fit. BMI is calculated using the weight divided by the height squared, where the weight in kg and height in m.People having BMI under 18.5 are underweight, from 18.5 to 25 are fit, and over 25 are overweight. Given Height and Weight.
+    
+2. A discount is made on a purchase as follows: 
+• if purchase ≤ 1000 L.E., there is no discount 
+• if purchase > 1000 L.E., the discount is 5% 
+Given the cost of the purchase, write an algorithm to calculate and print the money paid taking into consideration the 10% sales taxes. The taxes are calculated on the amount after the discount.
+    
+3. For the first two years, a dog year is equal to 10.5 human years. After that, each dog year is equal to 4 human years. Write an algorithm to calculate a dog's age in dog years, given a dog's age in human years. Expected output should look like the following: 
+• Input a dog’s age in human years: 2 The dog’s age in dog years is 21 2 
+• Input a dog’s age in human years: 15 The dog’s age in dog years is 73 
+• Input a dog’s age in human years: -5 Age must be positive number.
+    
+4. Write an algorithm that acts as a calculator for 2 numbers. Calculator should be able to add, subtract, multiply, and divide. Given Two Number and the sign of the arithmetic operation.
---
+++ problemSolving/part1/script.js
@@ -0,0 +1,242 @@
+function calculateSum() {
+    var number1 = Number(document.getElementById('number1-1').value);
+    var number2 = Number(document.getElementById('number2-1').value);
+    var sum = number1 + number2;
+    document.getElementById('result').innerText = 'Sum: ' + sum;
+}
+
+function calculateAverage() {
+    var num1 = Number(document.getElementById('number1-2').value);
+    var num2 = Number(document.getElementById('number2-2').value);
+    var num3 = Number(document.getElementById('number3-2').value);
+    var average = (num1 + num2 + num3) / 3;
+    document.getElementById('averageResult').innerText = 'Average: ' + average;
+}
+
+function calculateFlyingTime() {
+    var distance = Number(document.getElementById('distance').value);
+    var speed = Number(document.getElementById('speed').value);
+    if(speed <= 0) {
+        document.getElementById('flyingTimeResult').innerText = 'Invalid speed. Please enter a positive number.';
+        return;
+    }
+    var flyingTime = distance / speed;
+    var hours = Math.floor(flyingTime);
+    var minutes = Math.round((flyingTime - hours) * 60);
+    document.getElementById('flyingTimeResult').innerText = 'Flying Time: ' + hours + ' hour(s) and ' + minutes + ' minute(s)';
+}
+
+function calculateBMI() {
+    var weight = Number(document.getElementById('weight').value);
+    var height = Number(document.getElementById('height').value);
+    var bmi = weight / (height * height);
+    document.getElementById('bmiResult').innerText = 'BMI: ' + bmi.toFixed(2);
+}
+
+function convertLength() {
+    var lengthCm = Number(document.getElementById('lengthCm').value);
+    var lengthInMeters = lengthCm / 100;
+    var lengthInKilometers = lengthCm / 100000;
+    document.getElementById('lengthInMeters').innerText = 'Meters: ' + lengthInMeters;
+    document.getElementById('lengthInKilometers').innerText = 'Kilometers: ' + lengthInKilometers.toFixed(2);
+}
+
+function convertData() {
+    var dataBytes = Number(document.getElementById('dataBytes').value);
+    var dataInBits = dataBytes * 8;
+    var dataInGigabytes = dataBytes / (1024 * 1024 * 1024);
+    document.getElementById('dataInBits').innerText = 'Bits: ' + dataInBits.toLocaleString() + ' bits';
+    document.getElementById('dataInGigabytes').innerText = 'Gigabytes: ' + dataInGigabytes.toFixed(3) + ' GB';
+}
+
+function calculateUploadTime() {
+    var uploadSpeed = Number(document.getElementById('uploadSpeed').value);
+    var fileSize = Number(document.getElementById('fileSize').value);
+    var uploadSpeedMBps = uploadSpeed / 8;
+    var uploadTimeInSeconds = fileSize / uploadSpeedMBps;
+    var minutes = Math.floor(uploadTimeInSeconds / 60);
+    var seconds = Math.round(uploadTimeInSeconds % 60);
+    document.getElementById('uploadTimeResult').innerText = 'Upload Time: ' + minutes + ' minute(s) and ' + seconds + ' second(s)';
+}
+
+function calculateCircleProperties() {
+    var radius = Number(document.getElementById('radius').value);
+    var area = Math.PI * radius * radius;
+    var circumference = 2 * Math.PI * radius;
+    document.getElementById('circleArea').innerText = 'Area: ' + area.toFixed(2) + ' square units';
+    document.getElementById('circleCircumference').innerText = 'Circumference: ' + circumference.toFixed(2) + ' units';
+}
+
+function swapValues() {
+    var number1 = Number(document.getElementById('number1-3').value);
+    var number2 = Number(document.getElementById('number2-3').value);
+    document.getElementById('originalValues').innerText = 'Original Values:' + number1 + ',' + number2;
+    
+    var temp = number1;
+    number1 = number2;
+    number2 = temp;
+    
+    document.getElementById('swappedValues').innerText = 'Swapped Values: ' + number1 + ', ' + number2;
+}
+
+function printBiggerNumber() {
+    var number1 = Number(document.getElementById('number1').value);
+    var number2 = Number(document.getElementById('number2').value);
+    var result;
+    
+    if (number1 > number2) {
+        result = number1;
+    } else if (number2 > number1) {
+        result = number2;
+    } else {
+        result = "Both numbers are equal.";
+    }
+    document.getElementById('biggerNumberResult').innerText = 'Bigger Number: ' + result;
+}
+
+function checkNumber() {
+    var number = Number(document.getElementById('number1-4').value);
+    var resultText = 'Number is: ';
+    
+    if (number > 0) {
+        resultText += 'Positive';
+    } else if (number < 0) {
+        resultText += 'Negative';
+    } else {
+        resultText += 'Zero';
+    }
+    
+    document.getElementById('numberResult').innerText = resultText;
+}
+
+function checkDivisibility() {
+    var number = Number(document.getElementById('number1-5').value);
+    var resultText = 'Result: ';
+    
+    if (number % 5 === 0 && number % 11 === 0) {
+        resultText += 'The number is divisible by both 5 and 11.';
+    } else {
+        resultText += 'The number is not divisible by both 5 and 11.';
+    }
+    
+    document.getElementById('divisibilityResult').innerText = resultText;
+}
+
+function checkEvenOdd() {
+    var number = Number(document.getElementById('number1-6').value);
+    var resultText = 'Number is: ';
+    
+    if (number % 2 === 0) {
+        resultText += 'Even';
+    } else {
+        resultText += 'Odd';
+    }
+    
+    document.getElementById('evenOddResult').innerText = resultText;
+}
+
+function checkWithinRange() {
+    var rangeStart = Number(document.getElementById('rangeStart').value);
+    var rangeEnd = Number(document.getElementById('rangeEnd').value);
+    var numberToCheck = Number(document.getElementById('numberToCheck').value);
+    var resultText = 'Result: ';
+    
+    if (numberToCheck >= rangeStart && numberToCheck <= rangeEnd) {
+        resultText += 'The number is within the range.';
+    } else {
+        resultText += 'The number is not within the range.';
+    }
+    
+    document.getElementById('rangeResult').innerText = resultText;
+}
+
+function calculateBMI() {
+    var height = Number(document.getElementById('height1').value);
+    var weight = Number(document.getElementById('weight1').value);
+    var bmi = weight / (height * height);
+    var resultText = 'BMI Result: ';
+    
+    if (bmi < 18.5) {
+        resultText += 'Underweight (BMI: ' + bmi.toFixed(2) + ')';
+    } else if (bmi >= 18.5 && bmi <= 25) {
+        resultText += 'Fit (BMI: ' + bmi.toFixed(2) + ')';
+    } else if (bmi > 25) {
+        resultText += 'Overweight (BMI: ' + bmi.toFixed(2) + ')';
+    }
+    
+    document.getElementById('bmiResult1').innerText = resultText;
+}
+
+function calculateFinalAmount() {
+    var purchaseAmount = Number(document.getElementById('purchaseAmount').value);
+    var discountRate = 0.05;
+    var taxRate = 0.10;
+    var discountedAmount;
+    var finalAmount;
+    var resultText = 'Final Amount: ';
+
+    if (purchaseAmount > 1000) {
+        discountedAmount = purchaseAmount - (purchaseAmount * discountRate);
+    } else {
+        discountedAmount = purchaseAmount;
+    }
+
+    finalAmount = discountedAmount + (discountedAmount * taxRate);
+
+    resultText += finalAmount.toFixed(2) + ' L.E.';
+    
+    document.getElementById('finalAmount').innerText = resultText;
+}
+
+function calculateDogYears() {
+    var humanYears = Number(document.getElementById('humanYears').value);
+    var dogYears;
+    var resultText = "Dog's Age in Dog Years: ";
+
+    if (humanYears < 0) {
+        resultText += 'Age must be a positive number.';
+    } else if (humanYears <= 2) {
+        dogYears = humanYears * 10.5;
+        resultText += dogYears;
+    } else {
+        dogYears = 21 + ((humanYears - 2) * 4);
+        resultText += dogYears;
+    }
+
+    document.getElementById('dogYearsResult').innerText = resultText;
+}
+
+function performCalculation() {
+    var number1 = Number(document.getElementById('number1-7').value);
+    var number2 = Number(document.getElementById('number2-7').value);
+    var operation = document.getElementById('operation').value.trim();
+    var result;
+    var resultText = 'Result: ';
+
+    switch (operation) {
+        case '+':
+            result = number1 + number2;
+            break;
+        case '-':
+            result = number1 - number2;
+            break;
+        case '*':
+            result = number1 * number2;
+            break;
+        case '/':
+            if (number2 === 0) {
+                resultText += 'Cannot divide by zero.';
+                document.getElementById('calculationResult').innerText = resultText;
+                return;
+            }
+            result = number1 / number2;
+            break;
+        default:
+            resultText += 'Invalid operation.';
+            document.getElementById('calculationResult').innerText = resultText;
+            return;
+    }
+
+    resultText += result;
+    document.getElementById('calculationResult').innerText = resultText;
+}
