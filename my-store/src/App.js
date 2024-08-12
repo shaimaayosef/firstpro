ac3965d6d9095dba1864e03421781ca3b452428b
shaimaayosef <shimaa.sayed8888@gmail.com>
2024-08-12 22:05:58 +0300

    task lec17 12-8

--- my-store/src/App.js
+++ my-store/src/App.js
@@ -2,24 +2,61 @@
 import './App.css';
 //import useState
 import React, { useState } from 'react';
+import laptopImg from './laptop.jpg';
+import mobileImg from './mobile.jpg';
+import tabletImg from './tablet.jpg';
 
 function App() {
+  const [name,setName] =useState('shaimaa');
   var[blogs,setBlogs]=useState(['blog1','blog2','blog3'])
   console.log(blogs);
   const addBlog=()=>{
     setBlogs(['blog1','blog2','blog3','blog4'])
   }
+  const handleClick=()=>{
+    setName('mohamed')
+    console.log(name);
+  }
+  //make an array of products
+  const [products,setProducts]=useState([
+    {name:'laptop',price:1000,img:laptopImg},
+    {name:'mobile',price:500,img:mobileImg},
+    {name:'tablet',price:300,img:tabletImg},
+  ])
+  //function to delete all opjects in the array
+  const deleteAll=()=>{
+    setProducts([])
+  }
   return (
     <div className="App">
-      <h1>Blog List</h1>
+      <h1>{name}</h1>
+      <button onClick={handleClick}>Change Name</button>
+
+      <br/>
+
+      <h1>Blogs</h1>
       <ul>
-        {blogs.map((blog)=>(
-          <h1>{blog}</h1>
-        ))}
+      {blogs.map((blog, index)=>(
+        <h1 key={index}>{blog}</h1>
+      ))}
       </ul>
       <button onClick={addBlog}>Add Blog 4</button>
+
+      <br/>
+
+      <h1>Products</h1>
+      <div className='container'>
+      {products.map((product, index)=>(
+        <div className="product" key={index}>
+        <img src={product.img} alt={product.name+'-img'}/>
+        <h2>{product.name}</h2>
+        <h3>{product.price}</h3>
+        </div>
+      ))}
+      </div>
+      <button onClick={deleteAll}>Delete All</button>
     </div>
-  );
+    );
 }
 
 export default App;
